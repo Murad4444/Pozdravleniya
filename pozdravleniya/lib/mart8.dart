9a5d06fbc3201263dea467c3557a4f53fb7847c0
Cirrus CI <support@cirruslabs.org>
2026-03-04 08:32:39 +0000

    changed

--- pozdravleniya/lib/mart8.dart
+++ pozdravleniya/lib/mart8.dart
@@ -3,30 +3,34 @@ import 'package:flutter/material.dart';
 class Mart8Screen extends StatelessWidget {
   const Mart8Screen({super.key});
 
+  // Если тебе нужна функция для кнопки, опиши её здесь
+  void _opencard() {
+    
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        automaticallyImplyLeading: false, // ВАЖНО: отключаем стандартную стрелку слева
+        automaticallyImplyLeading: false, 
         actions: [
           IconButton(
-            icon: const Icon(Icons.arrow_back), // Твоя стрелочка
+            icon: const Icon(Icons.arrow_back),
             onPressed: () {
-              Navigator.pop(context); // Возврат назад
+              Navigator.pop(context);
             },
           ),
         ],
       ),
       body: Column(
-        // Это выравнивает вещи по ВЕРТИКАЛИ (сверху вниз)
         mainAxisAlignment: MainAxisAlignment.start, 
-        
-        // Это выравнивает вещи по ГОРИЗОНТАЛИ (по центру)
         crossAxisAlignment: CrossAxisAlignment.center, 
-        
         children: [
-          const SizedBox(height: 20), // Отступ от AppBar (чтобы не прилипало)
-          const ElevatedButton(onPressed: onPressed, child: child)
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: _opencard,
+            child: const Text("Открыть!"),
+          ),
         ],
       ),
     );
