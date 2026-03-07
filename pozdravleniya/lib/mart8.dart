8941787dff61c405bddfb2137225525c976a1ac9
Cirrus CI <support@cirruslabs.org>
2026-03-07 12:16:08 +0000

    last version of first app

--- pozdravleniya/lib/mart8.dart
+++ pozdravleniya/lib/mart8.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
 
-class Mart8Screen extends StatelessWidget {
+class Mart8Screen extends StatefulWidget {
   const Mart8Screen({super.key});
+
+  @override
+  State<Mart8Screen> createState() => _Mart8ScreenState();
+}
+
+class _Mart8ScreenState extends State<Mart8Screen> {
+  // 1. Переменная должна быть ТУТ, внутри State
+  bool _isImageVisible = false;
 
   // Если тебе нужна функция для кнопки, опиши её здесь
   void _opencard() {
-    print('hello it is a test'); 
+    setState(() {
+      _isImageVisible = true;
+      
+    }); 
     
   }
 
   // Если тебе нужна функция для кнопки, опиши её здесь
   void _closecard() {
-    print('hello it is a test'); 
+    _isImageVisible = false; 
     
   }
 
@@ -33,6 +44,7 @@ class Mart8Screen extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.start, 
         crossAxisAlignment: CrossAxisAlignment.center, 
         children: [
+
           const SizedBox(height: 20),
           ElevatedButton(
             onPressed: _opencard,
@@ -43,6 +55,14 @@ class Mart8Screen extends StatelessWidget {
             onPressed: _closecard,
             child: const Text("Закрыть!"),
           ),
+          const SizedBox(height: 20),
+
+          // Твоя картинка из папки assets
+        if (_isImageVisible) 
+              Image.asset(
+                'assets/my_image.jpg',
+                width: 200,
+              ),
         ],
       ),
     );
